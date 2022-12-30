be8bb026a5ca4296e15429aa3efb2cf248460d18
mnetke.dev <mnetke.dev@DESKTOP-0BP7HQF>
2022-12-30 14:59:26 +0530

    [Satish Netke]:Second code

--- src/den/first.java
+++ src/den/first.java
@@ -2,7 +2,10 @@ package den;
 
 public class first
 {
-public static void main(String[] args) {
+public static void main(String[] args)
+{
 	System.out.println("First Commit");
+	System.out.println("Another Code Implement");
 }
 }
+                                                                                                                                                                                                                                        
