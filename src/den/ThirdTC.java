c9eeaa6932cb522e7f083eace4f79f4253857606
mnetke.dev <mnetke.dev@DESKTOP-0BP7HQF>
2022-12-30 17:45:22 +0530

    # WARNING: head commit changed in the meantime
    Merge branch 'mindcorp'

---
+++ src/den/ThirdTC.java
@@ -0,0 +1,14 @@
+package den;
+
+public class ThirdTC
+{
+public static void main(String[] args)
+{
+	int mr[]= {52,12,69,65};
+	   
+	   System.out.println(mr[3]);
+	   System.out.println(mr.length);
+	   
+	
+}
+}
